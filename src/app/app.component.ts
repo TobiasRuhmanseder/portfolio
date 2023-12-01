dc4b0600d9cf889aab305ad8c4b919a2c2b05ff7
Tobias RUhmanseder <tobi.r1707@gmail.com>
2023-12-01 23:43:03 +0100

    add the aos framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  ngOnInit() {
+    AOS.init({
+duration: 650,
+offset: 350
+    });
+
+  }
+
+  ngAfterViewInit() {
+    setTimeout(
+      () => {
+        AOS.refresh();
+      }, 500)
+  }
 }
+
