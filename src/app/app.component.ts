50362e4cde51a1f83ac69578aa688378ee6e01c6
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2024-01-05 00:23:40 +0100

    update the project to angular version 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core/dist/lib/translate.service';
+import { TranslateService } from '@ngx-translate/core';
 import AOS from 'aos';
 
 
@@ -14,10 +14,10 @@ export class AppComponent implements OnInit {
 
   constructor(translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en');
+    translate.setDefaultLang('de');
 
      // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('en');
+    translate.use('de');
 }
 
   ngOnInit() {
